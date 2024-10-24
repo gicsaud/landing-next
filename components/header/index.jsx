47aab4c8baba108a243aa9bdae61dba9fe93db88
Giovanna Saud <gicsaud1410@gmail.com>
2024-10-23 21:11:25 -0300

    Criação do header.

---
+++ components/header/index.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "/public/images/logo.svg";
+import Styles from "./header.module.scss";
+
+const Header = () => {
+    return (
+    <div className={Styles.container}>
+        <div className={Styles.logotipo}>
+            <Image src ={Logo}></Image>
+        </div>
+        <div className={Styles.menu}>
+            <Link href="/">Home</Link>
+            <Link href="/">O que fazemos</Link>
+            <Link href="/">Cases</Link>
+        </div>
+        <div className={Styles.action}>
+            <button className={Styles.button}>Fale Conosco</button>
+        </div>
+    </div>
+
+    );
+};
+
+export default Header
