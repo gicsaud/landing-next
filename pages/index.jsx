45e0e222c185e7d38c8c43a4c91bf32fa4ad29b3
Giovanna Saud <gicsaud1410@gmail.com>
2024-10-24 21:14:47 -0300

    Terminando Welcome e Features.

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,14 @@
 import Header from "../components/header";
 import Welcome from "@/components/welcome";
 import styles from "./Home.module.scss";
+import Features from "@/components/features";
 
 export default function Home() {
   return (
   <div className={styles.container}>
     <Header />
     <Welcome />
+    <Features />
   </div>
   );
 }
